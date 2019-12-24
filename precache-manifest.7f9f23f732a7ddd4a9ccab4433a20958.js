924b065e0412052f61fe83452368b85d60c4e834
Alex Jose <alexjose4747@gmail.com>
2019-12-24 15:03:12 +0530

    Updates

---
+++ precache-manifest.7f9f23f732a7ddd4a9ccab4433a20958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ffbbd9cc682f0b1b0f16ef571b9c2e",
+    "url": "/sudoku-solver/index.html"
+  },
+  {
+    "revision": "f605782681c138a03d75",
+    "url": "/sudoku-solver/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2828eb07edb3d551a70b",
+    "url": "/sudoku-solver/static/js/2.98082c9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku-solver/static/js/2.98082c9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f605782681c138a03d75",
+    "url": "/sudoku-solver/static/js/main.805f0624.chunk.js"
+  },
+  {
+    "revision": "0006c28c93ff488abe65",
+    "url": "/sudoku-solver/static/js/runtime-main.19981e26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku-solver/static/media/logo.5d5d9eef.svg"
+  }
+]);
